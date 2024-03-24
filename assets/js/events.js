e9eb2d81d650be6f5f37ca5c713a8a14d44a192c
Abinash Selvarasu <123747322+AbiXnash@users.noreply.github.com>
2024-03-24 03:42:10 +0000

    Events Update

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,27 +1,195 @@
 const eventData = {
   1: [
     {
-      cluster: "Cluster 4",
-      title: "Event 1",
-      note: "Event Description ",
+      cluster: "Theta 2k24",
+      title: "Inaguration",
+      note: "9:30 a.m. - 10:30 a.m. | Venue: Auditorium",
       url_sastra: "#",
-      url_otherclg: "#",
     },
     {
-      cluster: "Cluster 3",
-      title: "Event 2",
-      note: "Event Description ",
+      cluster: "Strategia",
+      title: "Hey Guys Let's Go",
+      note: "9:00 a.m. - 12:00 p.m. | Venue: 303",
       url_sastra: "#",
-      url_otherclg: "#",
+    },
+    {
+      cluster: "Informatica",
+      note: "9:00 a.m. - 12:00 p.m. | Venue: LAB-1",
+      title: "Idea-Verse",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Robotics",
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      title: "Robo-Saga",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      title: "BasketBall",
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      title: "Feet and Hand",
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Access America",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 410",
+      title: "Capture Caption Captivate",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Equilibria",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 202",
+      title: "Ezhuthukol Vazhi em Mozhi",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Equilibria",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 203",
+      title: "Avai Anjaamai",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Electronica",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 406",
+      title: "Byte Bank",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      note: "10:00 a.m. - 2:00 p.m. | Venue: Ground",
+      title: "Musical Chair",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      note: "10:00 a.m. - 2:00 p.m. | Venue: 203",
+      title: "Lucky Corner",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Mathematica",
+      note: "10:00 a.m. - 4:00 p.m. | Venue: 212",
+      title: "Wonder Fest",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Equilibria",
+      note: "11:00 a.m. - 1:00 p.m. | Venue: 208",
+      title: "Naanga Vandha Mattum Podhum",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Biogenesis",
+      note: "11:00 a.m. - 1:00 p.m. | Venue: 110",
+      title: "Synapse Sculptors",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Informatica",
+      note: "2:00 p.m. - 5:00 p.m. | Venue: LAB-1",
+      title: "Mission Impossible",
+      url_sastra: "#",
     },
   ],
   2: [
     {
-      cluster: "Cluster 2",
-      title: "Event 3",
-      note: "Event Description ",
+      cluster: "Strategia",
+      note: "9:00 a.m. - 12:00 p.m. | Venue: 309",
+      title: "Sharp Mind",
       url_sastra: "#",
-      url_otherclg: "#",
+    },
+    {
+      cluster: "Optica",
+      note: "9:30 a.m. - 12:00 p.m. | Venue: 310",
+      title: "Squid Game",
+      url_sastra: "#",
+    },{
+      cluster: "Robotics",
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      title: "Robo-Saga",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      title: "Feet and Hand",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Access America",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 305",
+      title: "Bing Bang",
+      url_sastra: "#",
+    },{
+      cluster: "Electronica",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 406",
+      title: "Technical Pick and Talk",
+      url_sastra: "#",
+    },{
+      cluster: "Informatica",
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 402",
+      title: "Logic and Ladder",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      note: "10:00 a.m. - 3:00 p.m. | Venue: Ground",
+      title: "One Over Cricket",
+      url_sastra: "#",
+    },{
+      cluster: "Mathematica",
+      note: "10:00 a.m. - 4:00 p.m. | Venue: 509",
+      title: "Fun Island",
+      url_sastra: "#",
+    },{
+      cluster: "Biogenesis",
+      note: "11:00 a.m. - 1:00 p.m. | Venue: Drawing Hall",
+      title: "Model Exhibit",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Sportiva",
+      note: "11:00 a.m. - 1:00 p.m. | Venue: Ground",
+      title: "Powerlifting",
+      url_sastra: "#",
+    },{
+      cluster: "Equilibria",
+      note: "11:00 a.m. - 1:30 p.m. | Venue: 208",
+      title: "Ithu Kalavara Bhoomi",
+      url_sastra: "#",
+    },{
+      cluster: "Optica",
+      note: "2:00 p.m. - 4:30 p.m. | Venue: 504",
+      title: "Cognitiva Cracking",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Equilibria",
+      note: "2:00 p.m. - 4:30 p.m. | Venue: 208",
+      title: "Kandupadi Kandupadi",
+      url_sastra: "#",
+    },{
+      cluster: "Equilibria",
+      note: "2:00 p.m. - 4:30 p.m. | Venue: 208",
+      title: "Tamizhaatam",
+      url_sastra: "#",
+    },{
+      cluster: "Equilibria",
+      note: "2:00 p.m. - 4:30 p.m. | Venue: 208",
+      title: "Un Kurali Oor Kural",
+      url_sastra: "#",
+    },
+    {
+      cluster: "Equilibria",
+      note: "2:00 p.m. - 5:00 p.m. | Venue: Auditorium",
+      title: "Sirappu Pattimandram",
+      url_sastra: "#",
     },
   ],
   3: [
@@ -30,7 +198,80 @@ const eventData = {
       title: "Event 4",
       note: "Event Description ",
       url_sastra: "#",
-      url_otherclg: "#",
+    },
+    {
+      note: "9:00 a.m. - :00 p.m. | Venue: Ground",
+      title: "Football",
+      cluster: "Sportiva",
+      url_sastra: "#",
+    },{
+      note: "9:00 a.m. - 5:00 p.m. | Venue: Ground",
+      title: "Feet and Hand",
+      cluster: "Sportiva",
+      url_sastra: "#",
+    },{
+      note: "10:00 a.m. - 1:00 p.m. | Venue: 501",
+      title: "Roadies",
+      cluster: "Strategia",
+      url_sastra: "#",
+    },{
+      note: "10:00 a.m. - 1:00 p.m. | Venue: SRC",
+      title: "Escape Room",
+      cluster: "Electronica",
+      url_sastra: "#",
+    },{
+      note: "10:00 a.m. - 4:00 p.m. | Venue: 502",
+      title: "Math Pi-Rates",
+      cluster: "Mathematica",
+      url_sastra: "#",
+    },{
+      note: "11:00 a.m. - 12:00 p.m. | Venue: SRC",
+      title: "Humaniod Robots",
+      cluster: "Theta 2k24",
+      url_sastra: "#",
+    },{
+      note: "11:00 a.m. - 1:00 p.m. | Venue: 208",
+      title: "Enaiya Vachi Comedy Keemedy Pannalaye",
+      cluster: "Equilibria",
+      url_sastra: "#",
+    },
+    {
+      note: "11:00 a.m. - 1:00 p.m. | Venue: 208",
+      title: "Suthudhe Suthude",
+      cluster: "Equilibria",
+      url_sastra: "#",
+    },{
+      note: "11:00 a.m. - 1:00 p.m. | Venue: 208",
+      title: "Iyyoo Ipove Kanna Kattuthae",
+      cluster: "Equilibria",
+      url_sastra: "#",
+    },
+    
+    {
+      note: "1:00 p.m. - 3:00 p.m. | Venue: 416 ",
+      title: "Fun Max",
+      cluster: "Biogenesis",
+      url_sastra: "#",
+    },{
+      note: "2:00 p.m. - 3:00 p.m. | Venue: SRC",
+      title: "Valediction",
+      cluster: "R. Prabhakaran",
+      url_sastra: "#",
+    },{
+      note: "2:00 p.m. - 5:00 p.m. | Venue: 510",
+      title: "Code Hunt",
+      cluster: "Informatica",
+      url_sastra: "#",
+    },{
+      note: "2:00 p.m. - 5:00 p.m. | Venue: Ground",
+      title: "Uriadi",
+      cluster: "Sportiva",
+      url_sastra: "#",
+    },{
+      note: "3:00 p.m. - 5:00 p.m. | Venue: Ground",
+      title: "Tug of War",
+      cluster: "Sportiva",
+      url_sastra: "#",
     },
   ],
 };
@@ -90,9 +331,6 @@ function displayEvents(events) {
       const dateBox = document.createElement("div");
       dateBox.classList.add("ag-courses-item_date-box");
 
-      const startLabel = document.createTextNode("Conducted by: ");
-      dateBox.appendChild(startLabel);
-
       const dateSpan = document.createElement("span");
       dateSpan.classList.add("ag-courses-item_date");
       dateSpan.textContent = event.cluster;
@@ -105,20 +343,11 @@ function displayEvents(events) {
       const sastraBtn = document.createElement("button");
       sastraBtn.classList.add("button-85");
       sastraBtn.setAttribute("role", "button");
-      sastraBtn.textContent = " SASTRA ";
+      sastraBtn.textContent = " Register";
       sastraBtn.addEventListener("click", () =>
         openURL(event.url_sastra || "#"),
       ); // Use event.url or default to '#'
       registerBtnDiv.appendChild(sastraBtn);
-
-      const otherCollegeBtn = document.createElement("button");
-      otherCollegeBtn.classList.add("button-85");
-      otherCollegeBtn.setAttribute("role", "button");
-      otherCollegeBtn.textContent = "Other College";
-      otherCollegeBtn.addEventListener("click", () =>
-        openURL(event.url_otherclg || "#"),
-      ); // Set a default URL for Other College
-      registerBtnDiv.appendChild(otherCollegeBtn);
 
       dateBox.appendChild(registerBtnDiv);
 
